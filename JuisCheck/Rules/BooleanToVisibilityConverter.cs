02f9440b4fdd82eb752f5a24aabe399247702ea6
Roger Hunen <roger@hunen.net>
2018-12-23 06:42:17 +0100

    Code: make value converters more robust New: visibility value in case of false can be specified via parameter (default = Hidden)

--- JuisCheck/Rules/BooleanToVisibilityConverter.cs
+++ JuisCheck/Rules/BooleanToVisibilityConverter.cs
@@ -5,12 +5,20 @@ using System.Windows.Data;
 
 namespace JuisCheck.Rules
 {
-	[ValueConversion(typeof(bool), typeof(Visibility))]
+	[ValueConversion(sourceType: typeof(bool), targetType: typeof(Visibility))]
 	public class BooleanToVisibilityConverter : IValueConverter
 	{
 		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
 		{
-			return (bool)value ? Visibility.Visible : Visibility.Hidden;
+			Visibility falseVisibility = Visibility.Hidden;
+
+			if (parameter is string parameterStr) {
+				if (!string.IsNullOrEmpty(parameterStr)) {
+					falseVisibility = Enum.TryParse(parameterStr, true, out Visibility result) ? result : falseVisibility;
+				}
+			}
+
+			return (bool)value ? Visibility.Visible : falseVisibility;
 		}
  
 		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
--- JuisCheck/Rules/NullableDateTimeToStringConverter.cs
+++ JuisCheck/Rules/NullableDateTimeToStringConverter.cs
@@ -4,16 +4,20 @@ using System.Windows.Data;
 
 namespace JuisCheck.Rules
 {
-	[ValueConversion(typeof(DateTime?), typeof(string))]
+	[ValueConversion(sourceType: typeof(DateTime?), targetType: typeof(string))]
 	public class NullableDateTimeToStringConverter : IValueConverter
 	{
 		public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
 		{
-			if (value == null) {
-				return string.Empty;
+			if (value is DateTime valueDateTime) {
+				if (parameter is string parameterString) {
+					return (valueDateTime).ToString(parameterString);
+				} else {
+					return (valueDateTime).ToString("G");
+				}
 			}
 
-			return ((DateTime)value).ToString((parameter as string) ?? "G");
+			return string.Empty;
 		}
  
 		public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
